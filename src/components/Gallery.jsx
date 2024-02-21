a16a525e724791409732a2c7c885f79ae0a8edca
Harry Gardiner Lunar <harry@lunar.build>
2024-02-21 16:06:39 +0000

    Base gallery layout and images added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,26 @@ const Gallery = () => {
     return (
         <section id="gallery" className="gallery">
             <h2>Our Work</h2>
-            <div className="gallery_grid"></div>
+            <div className="gallery__grid">
+                {/* <div className="gallery__item"> */}
+                    <Image src="/gague.webp" alt="Gallery Image 1" width={400} height={300} />
+                {/* </div> */}
+                {/* <div className="gallery__item"> */}
+                    <Image src="/gas.webp" alt="Gallery Image 2" width={400} height={300} />
+                {/* </div> */}
+                {/* <div className="gallery__item"> */}
+                    <Image src="/hob.webp" alt="Gallery Image 3" width={400} height={300} />
+                {/* </div> */}
+                {/* <div className="gallery__item"> */}
+                    <Image src="/pipes.webp" alt="Gallery Image 4" width={400} height={300} />
+                 {/* </div> */}
+                {/* <div className="gallery__item"> */}
+                    <Image src="/pipes-2.webp" alt="Gallery Image 5" width={400} height={300} />
+                 {/* </div> */}
+                {/* <div className="gallery__item"> */}
+                    <Image src="/tap.webp" alt="Gallery Image 6" width={400} height={300} />
+                {/* </div> */}
+            </div>
         </section>
     )
 };
