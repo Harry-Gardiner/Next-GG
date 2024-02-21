2a1f53f436cfc582dcc5cf2691786a4d83f4c566
Harry Gardiner Lunar <harry@lunar.build>
2024-02-21 09:13:31 +0000

    Converted all files to .jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,6 +5,7 @@ const Gallery = () => {
     return (
         <section id="gallery" className="gallery">
             <h2>Our Work</h2>
+            <div className="gallery_grid"></div>
         </section>
     )
 };
