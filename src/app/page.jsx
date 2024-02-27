b0c3ac7634e421938f06e705e716d1ea6d0a9cd8
Harry Gardiner Lunar <harry@lunar.build>
2024-02-27 09:06:07 +0000

    Footer partial created and styling added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/Hero";
-import Header from "@/components/Header";
+import Header from "@/components/partials/Header";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Gallery from "@/components/Gallery";
 import Testimonials from "@/components/Testimonials";
 import Logos from "@/components/Logos";
 import Contact from "@/components/Contact";
+import Footer from "@/components/partials/Footer";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       <Logos />
       <Contact />
     </main>
+    <Footer />
     </>
   );
 }
---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__company-info">
+            <p>Company registration: 12345678</p>
+            <p>VAT number: 392 8052 36</p>
+            <p>Gas Safe reg: 1012</p>
+        </div>
+        <div className="footer__build">
+            Design and build by <a href="https://www.linkedin.com/in/harry-gardiner-795b2178/" target="_blank" rel="noreferrer">Harry</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
