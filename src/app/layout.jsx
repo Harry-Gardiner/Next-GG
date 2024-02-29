c3108b9edd7462577803dc168a4d672c0181acfe
Harry Gardiner Lunar <harry@lunar.build>
2024-02-29 08:36:42 +0000

    Updated meta title and description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 import "../styles/main.scss";
 
 export const metadata = {
-  title: "Gardiners Gas & Plumbing",
-  description: "Dorchester based gas and plumbing services. Servicing in Dorchester, Weymouth and surrounding areas.",
+  title: "Gardiners Gas & Plumbing | Dorchester, Weymouth & Surrounding Areas",
+  description: "Dorchester based gas and plumbing services. Servicing in Dorchester, Weymouth and surrounding areas. Contact us for a free quote. Services include boiler installation, boiler servicing, boiler repairs, gas safety checks, gas appliance installation, gas appliance servicing, gas appliance repairs, plumbing installation, plumbing repairs, bathroom installation, bathroom repairs, landlord gas safety checks, and more.",
 };
 
 export default function RootLayout({ children }) {
