f3782330ac4c3eef03493832058c8e843de8c815
Harry Gardiner Lunar <harry@lunar.build>
2024-04-04 08:36:53 +0100

    Added slide in animation to call btn

--- src/components/partials/CallBtn.jsx
+++ src/components/partials/CallBtn.jsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
+import { keyframes } from '@emotion/react';
+import Reveal from 'react-awesome-reveal';
+
+const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+function CustomAnimation({ children }) {
+  return <Reveal keyframes={customAnimation} className="call-btn">{children}</Reveal>;
+}
 
 const CallBtn = () => {
   return (
-    <a href="tel:1234567890" className="call-btn">
-      <Image src="/images/icon-phone-blue.svg" alt="Phone icon" width={35} height={35} />
-      Call
-    </a>
+    <CustomAnimation>
+      <a href="tel:1234567890">
+        <Image src="/images/icon-phone-blue.svg" alt="Phone icon" width={35} height={35} />
+        Call
+      </a>
+    </CustomAnimation>
   );
 };
 export default CallBtn;
